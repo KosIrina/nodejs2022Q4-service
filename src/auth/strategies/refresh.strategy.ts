027677eacbd29da4bd774e7076382ff6520ccc0d
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2023-02-27 17:24:15 +0300

    feat: add auth strategies

---
+++ src/auth/strategies/refresh.strategy.ts
@@ -0,0 +1,35 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import 'dotenv/config';
+import { JwtPayload, JwtPayloadWithRefresh } from '../types';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET_REFRESH_KEY,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(
+    req: Request,
+    payload: JwtPayload,
+  ): Promise<JwtPayloadWithRefresh> {
+    const refreshToken = req
+      ?.get('authorization')
+      ?.replace('Bearer', '')
+      .trim();
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
